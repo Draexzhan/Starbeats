15bdbd4b1ab72fdd47accabae1ba11b843055a9a
cysabi <47790183+cysabi@users.noreply.github.com>
2025-09-24 21:38:05 -0400

    star ring

--- Star.cs
+++ Star.cs
@@ -2,37 +2,35 @@ using Godot;
 using System;
 using System.Threading;
 
+
 public partial class Star : Area2D
 {
-	[Export] public TextureButton StarButton { get; set; }
+	public int LEADUP_SECONDS = 5;
+
+	[Export] public string inputAction;
 
 	[Signal] public delegate void StarClickedEventHandler();
 
 	public override void _Ready()
 	{
-		if (StarButton != null)
+		if (inputAction == null)
 		{
-			StarButton.Connect("pressed", new Callable(this, nameof(OnButtonPressed)));
+			GD.PrintErr("no event action");
 		}
 	}
 
-	private void OnButtonPressed()
+	public void SpawnNote()
 	{
-		GD.Print("Star clicked via TextureButton!"); // debug
-		EmitSignal(nameof(StarClicked));             // ping Main
+		AnimationPlayer player = GetNode<AnimationPlayer>("AnimationPlayer");
+		player.Play("playNote");
 	}
 
-	public void PlaySong()
-	{
-		//ThreadStart playing audio track
-		//start timer
-		//read txt file on each 4ths (Create method using Bpm to determine interval between note placements) 
-		//how will txt files be read?
-		//BPM = 140
-		// -- will signal new measure (insert logic to divide amount of lines in meas)
-		// 0010
-		// 1010
-		
-	}
-
+    public override void _Process(double delta)
+    {
+		if (Input.IsActionPressed(inputAction))
+		{
+			// calculate timing
+			EmitSignal(SignalName.StarClicked);
+		}
+    }
 }
